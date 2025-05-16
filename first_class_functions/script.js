0c155ce3bad41dd62334bbc0194377de9ecf44ad
hijanhv <128885110+Hijanhv@users.noreply.github.com>
2025-05-16 18:50:09 +0530

    Add first_class_functions folder

---
+++ first_class_functions/script.js
@@ -0,0 +1,33 @@
+//function statement Aka function declaration
+function greet() {
+  console.log("Hello, World!");
+}
+
+//function expression
+var b = function (param1, param2) {
+  console.log("b called");
+};
+// Define function a
+function a() {
+  console.log("a called");
+}
+
+a();
+b();
+
+// Define function xyz
+function xyz() {
+  console.log("xyz called");
+}
+
+xyz();
+
+//anonymous function
+
+//named function expression
+
+//difference between parameter and argument
+
+//first class function
+
+//arrow function
